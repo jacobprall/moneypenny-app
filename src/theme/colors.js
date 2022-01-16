2480b7ddae4192eb764b6d8980440d83795ff026
Jacob Prall <prall.jacob@gmail.com>
2022-01-16 17:26:56 -0600

    structure app

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '',
+  secondary: '',
+  textPrimary: '',
+  textTertiary: '',
+  textGrey: '',
+  borderColor: '',
+  backgroudnColor: '',
+  ratingColor: '',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#F9F9F9'
+};
+
+export default colors;
